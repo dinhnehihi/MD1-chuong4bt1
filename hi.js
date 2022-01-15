0eb2ac8bab83617364b264fa530f41a6d9d36d42
dinhnehihi <97022359+dinhnehihi@users.noreply.github.com>
2022-01-15 21:07:21 +0700

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,13 +1,15 @@
-let inputYear = prompt('Nhập năm: ');
-let inputMonth = prompt('Nhập tháng: ');
 let inputDate = prompt('Nhập ngày: ');
-let year = parseInt(inputYear);
-let month = parseInt(inputMonth);
+let inputMonth = prompt('Nhập tháng: ');
+let inputYear = prompt('Nhập năm: ');
 let date = parseInt(inputDate);
-if(date == 1 || date == 2 || date == 3 || date == 4 || date == 5 || date == 6 || date == 7 || date == 8 || date == 9) {
+let month = parseInt(inputMonth);
+let year = parseInt(inputYear);
+if (date === 1 || date === 2 || date === 3 || date === 4 || date === 5 || date === 6 || date === 7 || date === 8 || date === 9) {
   date = "0" + date;
 }
-if(month == 1 || month == 2 || month == 3 || month == 4 || month == 5 || month == 6 || month == 7 || month == 8 || month == 9) {
-  month = "0" + month;
+if (month === 1 || month === 2 || month === 3 || month === 4 || month === 5 || month === 6 || month === 7 || month === 8 || month === 9) {
+  date = "0" + date;
 }
-document.write('Hôm nay là: ' + date + "-" + month + "-" + year);
+let text = "Hôm nay là: " + date + "-" + month + "-" + year;
+let result = text.fontcolor("Green"); //Đổi màu đoạn văn bản
+document.write(result);
